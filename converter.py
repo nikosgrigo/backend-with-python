aa343852eb44ca87432dd9d652bbb8a23b738916
nikosgrigo <iis21084@uom.edu.gr>
2023-11-29 15:39:47 +0200

    Add unit test for functions

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import functions as f
 
-unit_choices = {'meters':['feet', 'yards'],
+UNIT_CHOICES = {'meters':['feet', 'yards'],
                 'celsius':["fahrenheit"],
                 'square meter':["acre"],
                 'kilogram':["ounce","pound"],
@@ -9,7 +9,7 @@ unit_choices = {'meters':['feet', 'yards'],
                 'joule':["calorie","watt"],
                 'meter per second':["feet per second","miles per hour"],}
 
-unitList =list(unit_choices.keys())
+unitList =list(UNIT_CHOICES.keys())
 
 def init():
 
@@ -23,22 +23,22 @@ def init():
       print(unitList)
       fromUnitString = input("Select one unit from the above list:").lower()
 
-      if dipslay_available_convertions(fromUnitString):
+      if dipslay_available_conversions(fromUnitString):
 
          toUnitString = input("Now please enter unit to convert to:").lower()
 
-         if is_valid_convertion(fromUnitString,toUnitString):
+         if is_valid_conversion(fromUnitString,toUnitString):
             measurement_converter(value,fromUnitString,toUnitString)
          else:
             print("Something went wrong!")
 
-def dipslay_available_convertions(unit:str):
+def dipslay_available_conversions(unit:str):
     '''This function 1.checks if current unit is available on the unit list (as key)
                      2.returns true if unit that user provided exists
     '''
 
-    if unit in unit_choices:
-      print(f"You can convert {unit} to: {unit_choices[unit]}")
+    if unit in UNIT_CHOICES:
+      print(f"You can convert {unit} to: {UNIT_CHOICES[unit]}")
       return True
     else:
       print(f"Oops sorry but this converter does not support {unit} unit yet!")
@@ -52,7 +52,7 @@ def get_user_input():
    except ValueError:
       print("Error message:Invalid input from user Please provide a number.")
 
-def is_valid_convertion(fromUnit:str,toUnit:str):
+def is_valid_conversion(fromUnit:str,toUnit:str):
    '''This function checks if the provided unit to convert exists in my list AND
      then checks if the unit to convert to is possible
      
@@ -62,13 +62,12 @@ def is_valid_convertion(fromUnit:str,toUnit:str):
                  toUnit - convert to this unit
    '''
 
-   if fromUnit in unit_choices and toUnit in unit_choices[fromUnit]:
+   if fromUnit in UNIT_CHOICES and toUnit in UNIT_CHOICES[fromUnit]:
       return True
    else:
       return False
 
 def measurement_converter(value,fromUnit,toUnit):
-   # print("Start convertion...")
    print("---------------------------")
 
    fromUnit = fromUnit.replace(" ","_")
@@ -85,6 +84,7 @@ def measurement_converter(value,fromUnit,toUnit):
         # Call the function
        result = getattr(f, function_name)(value)
        print(f'{value} in {fromUnit} is equal to {result} in {toUnit}')
+       return result
    else:
         print(f"Function '{function_name}' not found.")
 
--- test_converter.py
+++ test_converter.py
@@ -1,24 +1,52 @@
-import converter as m
+# import converter  as m
+# import unittest as u
 
-def test_is_valid_convertion():
-    #Valid units
-    assert m.is_valid_convertion("meters","feet") == True
-    assert m.is_valid_convertion("METERS","feet") == True
-    assert m.is_valid_convertion("meter PEr second","feet per second") == True
-    assert m.is_valid_convertion("pascal","BAR") == True
-    assert m.is_valid_convertion("pascal","pound per square inch") == True
+# class Tests(u.TestCase):
 
-    #Invalid units
-    assert m.is_valid_convertion("m","feet") == False
-    assert m.is_valid_convertion("axax","feet per second") == False
-    assert m.is_valid_convertion("pascal","skoko") == False
-    assert m.is_valid_convertion("258","bar") == False
-    assert m.is_valid_convertion("kilogram","6") == False
-    assert m.is_valid_convertion("","acre") == False
-    assert m.is_valid_convertion("celsius","") == False
-    assert m.is_valid_convertion("","") == False
+#     def test_is_valid_conversion(self):
+#         #Valid units
+#         self.assertTrue(m.is_valid_conversion("meters","feet"))
+#         self.assertTrue(m.is_valid_conversion("meter PEr second","feet per second"))
+#         self.assertTrue(m.is_valid_conversion("pascal","BAR"))
+#         self.assertTrue(m.is_valid_conversion("pascal","pound per square inch"))
 
-# Driver code
-if __name__ == "__main__":
-    test_is_valid_convertion()
+#         # #Invalid units
+#         self.assertFalse(m.is_valid_conversion("m","feet"))
+#         self.assertFalse(m.is_valid_conversion("axax","feet per second"))
+#         self.assertFalse(m.is_valid_conversion("pascal","skoko"))
+#         self.assertFalse(m.is_valid_conversion("258","bar"))
+#         self.assertFalse(m.is_valid_conversion("kilogram","6"))
+#         self.assertFalse(m.is_valid_conversion("","acre"))
+#         self.assertFalse(m.is_valid_conversion("celsius",""))
+#         self.assertFalse(m.is_valid_conversion("",""))
+
+#     def test_dipslay_available_conversions(self):
+#         #Valid units
+#         self.assertTrue(m.dipslay_available_conversions('meters'))
+#         self.assertTrue(m.dipslay_available_conversions('celsius'))
+#         self.assertTrue(m.dipslay_available_conversions('liter'))
+#         self.assertTrue(m.dipslay_available_conversions('pascal'))
+#         self.assertTrue(m.dipslay_available_conversions('meter per second'))
+
+#         #Invalid units
+#         self.assertFalse(m.dipslay_available_conversions(''))
+#         self.assertFalse(m.dipslay_available_conversions('me'))
+#         self.assertFalse(m.dipslay_available_conversions('pounds'))
+#         self.assertFalse(m.dipslay_available_conversions('watt'))
+  
+#     def test_measurement_converter(self):
+#         #Check celsius to fahrenheit convertion
+#         self.assertEqual(m.measurement_converter(35,"celsius","fahrenheit") == 95)
+#         self.assertEqual(m.measurement_converter(20,"celsius","fahrenheit") == 68)
+
+#         #Check meters to feet and meters to yards
+#         self.assertEqual(m.measurement_converter(20,"meters","feet") == 65.61)
+#         self.assertEqual(m.measurement_converter(10000,"meters","yards") == 10936.13)
+
+#         #Write more to cover all possible values and methods
+
+
+# # Driver code
+# if __name__ == '__main__':
+#     u.main()
      
